0a2b58b97cecf712ae771d5742c15f7021e61421
Patrick Teixeira <tp1002251@gmail.com>
2023-11-04 17:15:40 -0300

    criando o body e configurando o router

--- piap-project/src/main.jsx
+++ piap-project/src/main.jsx
@@ -2,9 +2,29 @@ import React from 'react'
 import ReactDOM from 'react-dom/client'
 import App from './App.jsx'
 import './index.css'
+import {createBrowserRouter, RouterProvider} from "react-router-dom";
+import Home from './pages/Home/Home.jsx';
+import Details from './pages/Details/Details.jsx';
+
+const router = createBrowserRouter([
+  {
+    path: "/",
+    element: <App/>,
+    children: [
+      {
+        path: "/",
+        element: <Home/>,
+      },
+      {
+        path: "/details",
+        element: <Details/>,
+      }
+    ]
+  }
+])
 
 ReactDOM.createRoot(document.getElementById('root')).render(
   <React.StrictMode>
-    <App />
+    <RouterProvider router={router}/>
   </React.StrictMode>,
 )
