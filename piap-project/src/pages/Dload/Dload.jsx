33588d2b676fd0cdd3b8d0622f4a9846a94558c7
Patrick Teixeira <tp1002251@gmail.com>
2023-11-05 16:56:27 -0300

    corrigindo um erro no page number e seu botao

--- piap-project/src/pages/Dload/Dload.jsx
+++ piap-project/src/pages/Dload/Dload.jsx
@@ -1,11 +1,18 @@
 import "./Dload.css";
 import { BsFillCloudDownloadFill} from 'react-icons/bs';
+import {useNavigate} from "react-router-dom"
 
 export const Dload = () => {
-  return (
+    const navigate = useNavigate();
+
+    const clickfim = () => {
+        navigate("/");
+    }
+
+    return (
     <div className="down">
         <h2>Download PDF</h2>
-        <BsFillCloudDownloadFill/>
+        <BsFillCloudDownloadFill onClick={clickfim}/>
     </div>
   )
 }
